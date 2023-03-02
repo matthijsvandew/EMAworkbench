5dab34d931673814ae9bbd56b246fa8ee3fd6e74
Thomas-4444 <125289385+Thomas-4444@users.noreply.github.com>
2023-03-02 23:25:36 +0100

    Uitgebreide code, ook aangemaakt file waartoe het dataframe wordt geschreven.
    Iteratie code af voor alle benodigde kolommen in dataframe. Ook geschreven naar demo file die we kunnen gebruiken voor de simulatie.

--- assignment_2/EPA1352-G13-A2/model/create_correct_dataframe2.py
+++ assignment_2/EPA1352-G13-A2/model/create_correct_dataframe2.py
@@ -1,5 +1,5 @@
 import pandas as pd
-#pip install --force-reinstall -v "openpyxl==3.1.0"
+import numpy as np
 
 df_roads = pd.read_csv('_roads3.csv')
 #print(df.head)
@@ -21,20 +21,31 @@ df_roads = df_roads.rename(columns={'lrp':'LRPName'})
 
 df_bmms = df_bmms.drop_duplicates(subset=['LRPName'])
 
-
 df_roads = df_roads.rename(columns={'lrp':'LRPName'})
-
 
 merge_inner = df_bmms.merge(df_roads, how = 'right',on=['LRPName','road'])
 
-print(merge_inner)
-print(len(merge_inner))
-print(merge_inner.columns)
+#print(merge_inner['lon_y'].isna().sum()) # Geen missing values!
+#print(merge_inner['lon_x'].isna().sum()) # 924 missing values!
+#print(merge_inner['lat_y'].isna().sum()) # Geen missing values!
+#print(merge_inner['lat_x'].isna().sum()) # 924 missing values
+#print(merge_inner['length'].isna().sum()) # 924 missing values. Voor missing values length oplossing: als length == NaN: vervang NaN door verschil chainage i en i+1. Dat kan omdat dit roads zijn en geen bridges. Voor bridges is namelijk geen NaN value bij length.
+#print(merge_inner['chainage_x'].isna().sum()) # 924 missing values
+#print(merge_inner['chainage_y'].isna().sum()) # Geen missing values!
+#print(merge_inner['type_x'].isna().sum()) # 924 missing values
+#print(merge_inner['name_x'].isna().sum()) # 924 missing values
+#print(merge_inner['type_y'].isna().sum()) # Geen missing values!
+#print(merge_inner['name_y'].isna().sum()) # Geen missing values
 
-print(merge_inner['lon_y'].isna().sum())
-print(merge_inner['lon_x'].isna().sum()) #924 missing values!
-print(merge_inner['lat_y'].isna().sum())
-print(merge_inner['lat_y'].isna().sum())
-print(merge_inner['length'].isna().sum()) #924 missing values. Voor missing values length oplossing: als length == NaN: vervang NaN door verschil chainage i en i+1. Dat kan omdat dit roads zijn en geen bridges. Voor bridges is namelijk geen NaN value bij length.
-print(merge_inner['chainage_x'].isna().sum()) #924 missing values
-print(merge_inner['chainage_y'].isna().sum())
+#print(merge_inner.length[2]) # kan allebei
+#print(merge_inner.loc[2,'length']) # kan allebei
+
+for i in range(len(merge_inner['length'])-1):
+    if np.isnan(merge_inner.loc[i,'length']) == True:
+        merge_inner.loc[i,'length'] = (merge_inner.chainage_y[i+1] - merge_inner.chainage_y[i])
+        #print(merge_inner.loc[i,'length'])
+
+merge_inner = merge_inner[['road','LRPName','length','chainage_y','lon_y','lat_y','type_y','name_y','condition']]
+print(merge_inner)
+
+merge_inner.to_csv(r"C:\Github\epa1352advancedsimulation\assignment_2\EPA1352-G13-A2\data\demo_try_self.csv",index=False)
