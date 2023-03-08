aa2d604512cce045c4cc197e6eb7c269bd66a682
Thomas-4444 <125289385+Thomas-4444@users.noreply.github.com>
2023-03-08 14:28:30 +0100

    bridge uitgeschakeld
    Bridge update

--- assignment_2/EPA1352-G13-A2/model/components.py
+++ assignment_2/EPA1352-G13-A2/model/components.py
@@ -88,11 +88,11 @@ class Bridge(Infra):
         else:
             self.delay_time = 0
 
-        self.dictionary_bridge = {'id': self.unique_id, 'caused_delay_time': self.delay_time, 'replication': self.model.replication,
-                           'scenario': self.model.scenario}
-        print(self.dictionary_bridge)
-        self.model.df_bridge = self.model.df_bridge.append(self.dictionary_bridge, ignore_index=True)
-        print(self.model.df_bridge)
+        #self.dictionary_bridge = {'id': self.unique_id, 'caused_delay_time': self.delay_time, 'replication': self.model.replication,
+                            'scenario': self.model.scenario}
+        # print(self.dictionary_bridge)
+        #self.model.df_bridge = self.model.df_bridge.append(self.dictionary_bridge, ignore_index=True)
+        # print(self.model.df_bridge)
 
         return self.delay_time
 
