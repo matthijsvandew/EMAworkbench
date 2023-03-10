57e5820e43f40de55991acc3e91ca69d833f093e
Thillenaar <tomhillenaar@live.nl>
2023-03-10 14:47:27 +0100

    max bridges

--- assignment_2/EPA1352-G13-A2/experiment/visuals_bridges.py
+++ assignment_2/EPA1352-G13-A2/experiment/visuals_bridges.py
@@ -6,9 +6,11 @@ import matplotlib.pyplot as plt
 scenario_8_data_bridges = pd.read_csv(r'../experiment\scenario8_bridge_results.csv')
 scenario_8_data_bridges = scenario_8_data_bridges.groupby('id')['caused_delay_time'].sum().reset_index()
 print(scenario_8_data_bridges)
+
 fig , ax = plt.subplots(1,1)
-
-
 sns.boxplot(data=scenario_8_data_bridges,x='caused_delay_time', ax=ax, color='r').set(title= 'Boxplot delay time due to bridges scenario 8');
 
-plt.show()
+#plt.show()
+
+scenario_8_data_bridges_max = scenario_8_data_bridges.loc[scenario_8_data_bridges['caused_delay_time'].idxmax()]
+print(scenario_8_data_bridges_max)
