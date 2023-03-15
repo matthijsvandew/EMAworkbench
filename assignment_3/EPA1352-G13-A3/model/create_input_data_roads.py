2360c18be0c94a671639a0344c5f6cbbbfaf3c4d
Thillenaar <tomhillenaar@live.nl>
2023-03-15 12:36:48 +0100

    selection roads 1.0

--- assignment_3/EPA1352-G13-A3/model/create_input_data_roads.py
+++ assignment_3/EPA1352-G13-A3/model/create_input_data_roads.py
@@ -5,16 +5,6 @@ import random
 df_roads = pd.read_csv(r'../data\_roads3.csv')
 df_bmms = pd.read_excel('../data\BMMS_overview.xlsx')
 
-df_bmms = df_bmms.drop(df_bmms[df_bmms['road'] != 'N1'].index)
-df_roads = df_roads.drop(df_roads[df_roads['road'] != 'N1'].index)
-
-#These lines of codes could be used in order to delete 'Left bridges' out of the data set for this specific assignment.
-#However, this is not representitive when this model would be applied for more than just one route and, therefore, this line of code is shown in markdown format.
-#This subject is further explained in the discussion of the report.
-# df_bmms = df_bmms[df_bmms["name"].str.contains("(L)") == False]
-# df_bmms = df_bmms[df_bmms["name"].str.contains("( L )") == False]
-
-
 for i in range(len(df_roads['chainage'])): #chainage omzetten naar meters, want dat is lengte ook.
      df_roads.loc[i,'chainage'] = (df_roads.loc[i,'chainage'] * 1000)
 
@@ -27,20 +17,8 @@ df_bmms = df_bmms.drop_duplicates(subset=['LRPName'])
 
 merge_right = df_bmms.merge(df_roads, how = 'right',on=['LRPName','road'])
 
-#Check which one must be selected for the final dataset
-#print(merge_right['lon_y'].isna().sum()) # No missing values!
-#print(merge_right['lon_x'].isna().sum()) # 924 missing values!
-#print(merge_right['lat_y'].isna().sum()) # No missing values!
-#print(merge_right['lat_x'].isna().sum()) # 924 missing values
-#print(merge_right['length'].isna().sum()) # 924 missing values. --> Must be fixed. solution: if length == NaN: swap NaN by difference in chainage i and i+1. This is possible becasue they are roads and not bridges. Bridges doesn't contain Nan values for length.
-#print(merge_right['chainage_x'].isna().sum()) # 924 missing values
-#print(merge_right['chainage_y'].isna().sum()) # No missing values!
-#print(merge_right['type_x'].isna().sum()) # 924 missing values
-#print(merge_right['name_x'].isna().sum()) # 924 missing values
-#print(merge_right['type_y'].isna().sum()) # No missing values!
-#print(merge_right['name_y'].isna().sum()) # No missing values
-#print(merge_right.length[2]) # both possible
-#print(merge_right.loc[2,'length']) # Both possible
+merge_right = merge_right[(merge_right["road"].str.contains("N1") == True) | (merge_right["road"].str.contains("N2") == True)]
+
 
 for i in range(len(merge_right['length'])-1): #when length is nan, take difference in chainage
     if np.isnan(merge_right.loc[i,'length']) == True:
@@ -89,13 +67,6 @@ for i in range(len(merge_right['condition'])):
                else:
                     merge_right.loc[i, 'condition'] = 'D'
 
-# count=0
-# for i in range(len(merge_right['condition'])):
-#      if merge_right.loc[i, 'type_y'] == 'Culvert' or merge_right.loc[i, 'type_y'] == 'Bridge':
-#           if pd.isna(merge_right.loc[i, 'condition']) == True:
-#                count+=1
-# print(count)
-
 
 merge_right.insert(loc=0,column='id',value=0)
 
@@ -103,8 +74,6 @@ for i in range(len(merge_right)):
      merge_right.loc[i, 'id'] = round(i)
      #print(i)
      #print(int(i))
-
-merge_right = merge_right.loc[merge_right['id'] <= 502]
 
 for i in range(len(merge_right)):
      #Start at Chittagong
