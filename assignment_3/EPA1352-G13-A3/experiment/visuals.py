2fca2ffab3945bbae4b932036a212a44d0a9bc7b
Yassay90559 <125289794+Yassay90559@users.noreply.github.com>
2023-03-24 00:07:43 +0100

    Visuals
    Visuals

---
+++ assignment_3/EPA1352-G13-A3/experiment/visuals.py
@@ -0,0 +1,19 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+Base_scenario = pd.read_csv(r'../experiment\base_case_results.csv')
+scenario_1_data = pd.read_csv(r'../experiment\scenario1_results.csv')
+scenario_2_data = pd.read_csv(r'../experiment\scenario2_results.csv')
+scenario_3_data = pd.read_csv(r'../experiment\scenario3_results.csv')
+scenario_4_data = pd.read_csv(r'../experiment\scenario4_results.csv')
+
+fig , ax = plt.subplots(2,3, figsize=(17,10))
+
+sns.boxplot(data=Base_scenario,x='drive_time', ax=ax[0,0], color='r').set(title= 'Boxplot driving time base scenario');
+sns.boxplot(data=scenario_1_data,x='drive_time', ax=ax[0,1], color='r').set(title= 'Boxplot driving time scenario 1');
+sns.boxplot(data=scenario_2_data,x='drive_time', ax=ax[0,2], color='r').set(title= 'Boxplot driving time scenario 2');
+sns.boxplot(data=scenario_3_data,x='drive_time', ax=ax[1,0], color='r').set(title= 'Boxplot driving time scenario 3');
+sns.boxplot(data=scenario_4_data,x='drive_time', ax=ax[1,1], color='r').set(title= 'Boxplot driving time scenario 4');
+
+plt.show()
