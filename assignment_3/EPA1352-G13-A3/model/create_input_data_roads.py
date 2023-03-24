800f4287222c69d2789a21fd21455bb109242a70
Thomas-4444 <125289385+Thomas-4444@users.noreply.github.com>
2023-03-24 13:44:26 +0100

    Update create_input_data_roads.py
    Create comments last part.

--- assignment_3/EPA1352-G13-A3/model/create_input_data_roads.py
+++ assignment_3/EPA1352-G13-A3/model/create_input_data_roads.py
@@ -5,10 +5,10 @@ import random
 df_roads = pd.read_csv(r'../data\_roads3.csv')
 df_bmms = pd.read_excel('../data\BMMS_overview.xlsx')
 
-for i in range(len(df_roads['chainage'])): #chainage omzetten naar meters, want dat is lengte ook.
+for i in range(len(df_roads['chainage'])): # Convert chainage to meters, because length is also in meters.
      df_roads.loc[i,'chainage'] = (df_roads.loc[i,'chainage'] * 1000)
 
-df_roads = df_roads.drop(df_roads[df_roads['gap'] == 'BE'].index)
+df_roads = df_roads.drop(df_roads[df_roads['gap'] == 'BE'].index) # Only include one node/location for the bridges.
 
 df_roads = df_roads.drop(columns='gap')
 
@@ -17,19 +17,20 @@ df_bmms = df_bmms.drop_duplicates(subset=['LRPName'])
 
 merge_right = df_bmms.merge(df_roads, how = 'right',on=['LRPName','road'])
 
+# Keep al the rows in the dataframe that are about the N1 and N2 with its side roads.
 merge_right = merge_right[(merge_right["road"].str.contains("N1") == True) | (merge_right["road"].str.contains("N2") == True)]
 
-for i in range(len(merge_right['length'])-1): #when length is nan, take difference in chainage
+for i in range(len(merge_right['length'])-1): # When length is NaN, take difference in chainage as length.
     if np.isnan(merge_right.loc[i, 'length']) == True:
         merge_right.loc[i,'length'] = (merge_right.chainage_y[i+1] - merge_right.chainage_y[i])
 
-merge_right = merge_right[['road','LRPName','length','chainage_y','lon_y','lat_y','type_y','name_y','condition']] #only necessary columns.
+merge_right = merge_right[['road','LRPName','length','chainage_y','lon_y','lat_y','type_y','name_y','condition']] # Keep only necessary columns.
 
 for i in range(len(merge_right['road'])-1):
     if merge_right.loc[i, 'road'] != merge_right.loc[i+1, 'road']:
         type_road = merge_right.loc[i, 'road']
         length_road = merge_right.chainage_y[i]
-        if length_road < 25000:
+        if length_road < 25000: # If the length of a side road of the N1 or N2 is less than 25km, we do not take the side road into account.
              merge_right = merge_right.drop(merge_right[merge_right['road'] == type_road].index)
 
 merge_right = merge_right.reset_index(drop=True)
@@ -63,6 +64,7 @@ a = random.random()
 b = random.random()
 c = random.random()
 
+# For bridges (and culverts) that do not have a condition yet, specify the condition based on the current distribution of condition for bridges in the dataset (see above).
 for i in range(len(merge_right['condition'])):
      if merge_right.loc[i, 'type_y'] == 'Culvert' or merge_right.loc[i, 'type_y'] == 'Bridge':
           if pd.isna(merge_right.loc[i, 'condition']) == True:
@@ -77,8 +79,10 @@ for i in range(len(merge_right['condition'])):
 
 merge_right.insert(loc=0,column='id',value=0)
 
-for i in range(len(merge_right)):
+for i in range(len(merge_right)): # Give each row in the dataframe a proper id number.
      merge_right.loc[i, 'id'] = round(i)
+
+# Specify the types of elements in the network.
 
 for i in range(len(merge_right)):
      if i == len(merge_right)-1:
@@ -106,6 +110,7 @@ for i in range(1, len(merge_right)-1):
 
 merge_right= merge_right.rename(columns={'type_y':'model_type','lat_y':'lat','lon_y':'lon','name_y':'name','chainage_y':'chainage'})
 
+# Insert an extra link between bridges if the bridges follow each other directly. Note that this is not really necessary as bridges are nodes, with their lengths as links already (see road_graph_nx.py).
 for i in range(len(merge_right)-1):
      if merge_right.loc[i, 'model_type'] == 'bridge' and merge_right.loc[i+1, 'model_type'] == 'bridge':
           merge_right.loc[(i+0.5)] = [(i+1), merge_right.road[i], 'LRP_inserted', 0, 0, ((merge_right.lon[i] + merge_right.lon[i+1])/2), ((merge_right.lat[i] + merge_right.lat[i+1])/2), 'link', 'inserted_link', 'NaN']
@@ -119,6 +124,7 @@ merge_right.insert(loc=0,column='id',value=0)
 for i in range(len(merge_right)):
      merge_right.loc[i, 'id'] = round(i)
 
+# Integrate intersections in the dataframe that should be in the data already, but are not. These intersections exist, but are not in the data.
 merge_right.loc[(565+0.5)] = [(565+1), merge_right.road[565], 'LRP_inserted', 0, 0, 91.833060, 22.368996, 'intersection', 'inserted_intersection', 'NaN']
 merge_right.loc[(1938+0.5)] = [(565+1), merge_right.road[1939], 'LRP_inserted', 0, 0, 91.833060, 22.368996, 'intersection', 'inserted_intersection', 'NaN']
 
@@ -131,9 +137,9 @@ merge_right.insert(loc=0,column='id',value=0)
 for i in range(len(merge_right)):
      merge_right.loc[i, 'id'] = round(i)
 
-#Some sourcesinks need to be intersections points
-#1939 thus 1941
-merge_right.loc[1941,['model_type']] = ['link']
+# Some (extra) sourcesinks/links need to be intersections points. This is determined based on the information about each LRP given in the dataframe and Google Maps.
+
+merge_right.loc[1941,['model_type']] = ['link'] # 1940 becomes an intersection. 1941 needs to be a link.
 merge_right.loc[1477,['model_type']] = ['intersection']
 merge_right.loc[1691,['model_type']] = ['intersection']
 merge_right.loc[1687,['model_type']] = ['intersection']
@@ -158,6 +164,7 @@ merge_right.loc[3664,['id','lon','lat']] = ['3058','91.949583','24.9163056'] # i
 merge_right.loc[3462,['id','lon','lat']] = ['3412','91.7654722','24.4714438'] # intersection N207 N208
 merge_right.loc[3714,['id','lon','lat']] = ['3657','91.896583','24.8479997'] # intersection N208 N210
 
+# To reduce the number of nodes: take all links that follow each other directly as one link (road section) with as total length the sum of all the parts.
 for i in range(0, len(merge_right)-1):
      if i == len(merge_right):
           break
@@ -170,6 +177,7 @@ for i in range(0, len(merge_right)-1):
                     i = i+1
                merge_right = merge_right.reset_index(drop=True)
 
+# No negative lengths.
 for i in range(len(merge_right)):
      if merge_right.loc[i, 'length'] < 0:
           merge_right.loc[i, 'length'] = 0
