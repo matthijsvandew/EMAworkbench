27fffa949e3a46b48da9534257368faf6c1d3dd5
thomassandbergen <thomassandbergen@gmail.com>
2023-03-29 12:00:01 +0200

    Created AADT 5
    This is the correct file which we probably should use. However that needs to be validated. Test.py shows how we got that file.

--- assignment_4/EPA1352-G13-A4/model/test.py
+++ assignment_4/EPA1352-G13-A4/model/test.py
@@ -2,7 +2,21 @@ import pandas as pd
 
 file_name = ('../data\elevant_N_roads_AADT_overview4.csv')
 
-df = pd.read_csv(file_name)
+df = pd.read_csv(file_name,index_col=False)
+
+print(df.head())
+
+print(df.columns)
+
+df = df.drop(columns=['(AADT)'])
+
+print(df.head())
+
+df.to_csv('../data\elevant_N_roads_AADT_overview5.csv',index=False)
+
+file_name = ('../data\elevant_N_roads_AADT_overview5.csv')
+
+df = pd.read_csv(file_name,index_col=False)
 
 print(df.head())
 
