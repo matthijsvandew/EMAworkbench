7fd8de7f10109b52419a5146cab42c2ae1477b19
thomassandbergen <thomassandbergen@gmail.com>
2023-03-29 12:04:41 +0200

    Deleted old AADT files

--- assignment_4/EPA1352-G13-A4/model/test.py
+++ assignment_4/EPA1352-G13-A4/model/test.py
@@ -1,20 +1,6 @@
 import pandas as pd
 
-file_name = ('../data\elevant_N_roads_AADT_overview4.csv')
-
-df = pd.read_csv(file_name,index_col=False)
-
-print(df.head())
-
-print(df.columns)
-
-df = df.drop(columns=['(AADT)'])
-
-print(df.head())
-
-df.to_csv('../data\elevant_N_roads_AADT_overview5.csv',index=False)
-
-file_name = ('../data\elevant_N_roads_AADT_overview5.csv')
+file_name = ('../data\AADT_N_roads_overview.csv')
 
 df = pd.read_csv(file_name,index_col=False)
 
